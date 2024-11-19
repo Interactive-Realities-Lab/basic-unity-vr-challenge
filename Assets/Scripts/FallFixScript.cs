0b889c57a93179b88d60d94f7980f7da1324bd26
A_Ariannejad <ariannejad14@gmail.com>
2024-11-19 20:35:19 +0330

    Added: the new Shotgun Rifle

--- Assets/Scripts/FallFixScript.cs
+++ Assets/Scripts/FallFixScript.cs
@@ -5,6 +5,5 @@ public class FallFixScript : MonoBehaviour
     private void OnCollisionStay(Collision collision)
     {
         collision.gameObject.transform.position = new Vector3(collision.gameObject.transform.position.x, 0.5f, collision.gameObject.transform.position.y);
-        Debug.LogError("KIIIIR");
     }
 }
